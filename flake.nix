86c926c490995eaccd7c798b6af79e855d6a829c
Pravdyvy <awxrvtb@gmail.com>
2026-06-15 16:18:53 +0300

    fix(nix): lez module version update

--- flake.nix
+++ flake.nix
@@ -4,7 +4,7 @@
   inputs = {
     logos-module-builder.url = "github:logos-co/logos-module-builder";
     nix-bundle-lgx.url = "github:logos-co/nix-bundle-lgx";
-    logos_execution_zone.url = "github:logos-blockchain/logos-execution-zone-module?rev=09f539a13662ae1a177e66cceb42a881307d92ca";
+    logos_execution_zone.url = "github:logos-blockchain/logos-execution-zone-module?rev=1eb03cedfb00def12a6d0e415561d3c76c3b16bf";
   };
 
   outputs = inputs@{ logos-module-builder, ... }:
