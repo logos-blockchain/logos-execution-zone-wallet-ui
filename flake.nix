50f1dbbef6f301835670d53005bbbc908d7ec4ee
erhant <erhany96@gmail.com>
2026-09-07 22:07:27 +0300

    chore: pin latest LEZ core

--- flake.nix
+++ flake.nix
@@ -10,8 +10,7 @@
   inputs = {
     logos-module-builder.url = "github:logos-co/logos-module-builder";
     nix-bundle-lgx.url = "github:logos-co/nix-bundle-lgx";
-    # FIXME: pinned to the fees FFI wiring branch; re-pin to main once merged upstream.
-    lez_core.url = "git+https://github.com/logos-blockchain/logos-execution-zone-module?ref=erhant/wallet-fees-wiring";
+    lez_core.url = "github:logos-blockchain/logos-execution-zone-module";
   };
 
   outputs = inputs@{ logos-module-builder, ... }:
